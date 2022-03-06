a728a79ffdf920b9780a8552129a30196aad3cd1
purvirahulmehta <purvirahulmehta@gmail.com>
2022-03-06 21:21:26 +0000

    homework java week 5

---
+++ src/homework5/Programme1.java
@@ -0,0 +1,37 @@
+package homework5;
+
+public class Programme1 {
+    public static void main(String[] args) {
+        System.out.println("                   Biodata                   ");
+        System.out.print("Name : ");
+        System.out.print("    Mrs");
+        System.out.print(" Pooja");
+        System.out.println(" Shah");
+        System.out.print("Address : ");
+        System.out.print(" Sweet Home,");
+        System.out.println(" Harrow .");System.out.print("Postcode :");
+        System.out.println(" HA23DS");
+        System.out.print("Contact No:");
+        System.out.println("07878453943");
+        System.out.println("================================================");
+        System.out.println("Hobbies :");
+        System.out.print("Reading,");
+        System.out.print("Gardening ,");
+        System.out.print("Watching Movie ,");
+        System.out.println("Cooking ");
+        System.out.println("==================================================");
+        System.out.println("Visited Country");
+        System.out.println("India");
+        System.out.println("UAE");
+        System.out.println("France");
+        System.out.println("Sri Lanka");
+        System.out.println("USA");
+        System.out.println("====================================================");
+        System.out.print("Thank you");
+
+
+
+
+                }
+                }
+
